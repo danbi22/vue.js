ae30bbe0dbd1ec4f6f5d0642bcc337ef2afa943d
dahan <dbekgks98@gmail.com>
2023-11-09 17:26:34 +0900

    vue.js

--- learn-vue-js-master/vue3-cli/src/App.vue
+++ learn-vue-js-master/vue3-cli/src/App.vue
@@ -1,35 +1,19 @@
 <template>
-  <AppHeader 
-    v-bind:appTitle="message"
-    v-on:change="changeMessage">
-  </AppHeader>
+  <div>
+    app
+  </div>
 </template>
 
 <script>
-// import 컴포넌트이름 from './컴포넌트 경로'
-import AppHeader from './components/AppHeader.vue'
-
 export default {
-  components: {
-    // '컴포넌트이름': 컴포넌트 내용
-    // 'app-header': AppHeader,
-    // 'AppHeader': AppHeader,
-    // AppHeader: AppHeader,
-    AppHeader
-  },
-  data() {
+  data: function() {
     return {
-      message: '앱 헤더 컴포넌트'
-    }
-  },
-  methods: {
-    changeMessage() {
-      this.message = '변경됨'
+      str: 'hi'
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
 
 </style>
