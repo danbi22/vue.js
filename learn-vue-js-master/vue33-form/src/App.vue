8c520ef878102bc5cd78cd18d5da8f10841e2775
dahan <dbekgks98@gmail.com>
2023-11-13 16:50:58 +0900

    vue3

---
+++ learn-vue-js-master/vue33-form/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID: </label>
+      <input id="userId" type="text" v-model="userId">
+    </div>
+    <div>
+      <label for="password">PW: </label>
+      <input id="password" type="text" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+  import axois from 'axios'
+  import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const username = ref('');
+      const password = ref('');
+    }
+    // data() {
+    //   return {
+    //     userId: '',
+    //     password: '',
+    //   }
+    // },
+    // methods: {
+    //   submitForm() {
+    //     axois.post('https://jsonplaceholder.typicode.com/users/', {
+    //       userId: this.userId,
+    //       password: this.password,
+    //     })
+    //     .then(response => console.log(response))
+    //     .catch(error => console.log(error));
+    //     console.log('제출됨');
+    //   }
+    // }
+  }
+</script>
+
+<style scoped>
+
+</style>
