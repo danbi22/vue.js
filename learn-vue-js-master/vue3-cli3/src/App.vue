b209bfed742956829d2836884e71eadcd1834231
dahan <dbekgks98@gmail.com>
2023-11-13 15:42:44 +0900

    vue3

---
+++ learn-vue-js-master/vue3-cli3/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <app-header
+    @changee="change"
+    :appTitle="message">
+  </app-header>
+  <div>
+    {{ message }}
+  </div>
+  <button @click="showAlert">경고</button>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+  export default {
+    components: {
+      AppHeader
+    },
+    data() {
+      return {
+        message: '앱 헤더 컴포넌트'
+      }
+    },
+    methods: {
+      showAlert() {
+        alert('hello');
+      },
+      change() {
+        console.log('change()');
+      }
+    },
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ learn-vue-js-master/vue3-cli3/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ appTitle }}</h1>
+  <button @click="change">change</button>
+</template>
+
+<script>
+  export default {
+    props: ['appTitle'],
+    methods: {
+      change() {
+        this.$emit('changee')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
